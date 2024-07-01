a5a0aea212adf7f50d6128bda697db68902c56b1
Amedamin <ahmedqryqr@gmail.com>
2024-06-30 22:45:08 -0700

    yes

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const GRADE_Obj = newGrades.find(grade => grade.studentId === student.id);
+      const GREADE = GRADE_Obj ? GRADE_Obj.grade : 'N/A';
+      return { ...student, grade: GREADE };
+    });
+}
